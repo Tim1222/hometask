6ffc6a59fa031014c0f4d4cf17fbd5d7547e8b76
Tim1222 <timofeu47@gmail.com>
2023-04-30 19:24:32 +0300

    Updates

---
+++ precache-manifest.81bdcf6c844e3fc80a3f1473db58a57c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba55ac89b2750250b5e",
+    "url": "/hometask/static/css/main.dd5185ab.chunk.css"
+  },
+  {
+    "revision": "2ba55ac89b2750250b5e",
+    "url": "/hometask/static/js/main.2ba55ac8.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/hometask/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "4730e30500a29071c933",
+    "url": "/hometask/static/js/2.4730e305.chunk.js"
+  },
+  {
+    "revision": "e0706f3dc4963a6a9070",
+    "url": "/hometask/static/js/runtime~main.e0706f3d.js"
+  },
+  {
+    "revision": "a9eea824600d2827e1735b8594aed9f8",
+    "url": "/hometask/index.html"
+  }
+];
